37e49ecd20845b20feca9b1d56d73c6a406130c6
Mark Kang <sk531@duke.edu>
2019-02-19 15:39:10 -0500

    module for visualization created

---
+++ src/Visualization/TurtleIDE.java
@@ -0,0 +1,5 @@
+package Visualization;
+
+public class TurtleIDE {
+    
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module game.sk531 {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+
+    exports main;
+}
