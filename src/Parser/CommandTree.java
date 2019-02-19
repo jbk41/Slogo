2972710da7c2ee83157280a61549b9f3b476ec1b
Justin Kim <jbk41@duke.edu>
2019-02-19 13:10:55 -0500

    added some incomplete methods

--- src/Parser/CommandTree.java
+++ src/Parser/CommandTree.java
@@ -1,17 +1,32 @@
 package Parser;
-import CommandNode.Node;
+import CommandNode.*;
+
 
 
 public class CommandTree {
 
     private String[] myText;
+    private int myTextIndex;
     private Node head;
+    private NodeMaker myNM;
 
     public CommandTree(String text){
         myText = text.split("\\s+");
+        myNM = new NodeMaker();
+        myTextIndex = 0;
+    }
+
+    private void insertHead(){
+        head = myNM.makeNode(myText[myTextIndex]);
+        myTextIndex++;
     }
 
     protected void parse(){
+        insertHead();
+        Node ptr = head;
+        while (myTextIndex < myText.length){
+            if 
+        }
 
     }
 
