d649802ba51952e136476180cc59ca4f95c2bc03
Bryant Huang <bh179@duke.edu>
2019-02-23 17:11:42 -0500

    working on help

--- src/Visualization/TurtleIDE.java
+++ src/Visualization/TurtleIDE.java
@@ -3,6 +3,7 @@ package Visualization;
 import TurtleData.TextReader;
 import TurtleData.TextWriter;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -10,16 +11,20 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TurtleIDE extends Application {
     private static final String title = "Turtle IDE";
@@ -104,7 +109,11 @@ public class TurtleIDE extends Application {
         help.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-
+                Popup popup = new Popup();
+                //TODO: create an instruction class and fill List with Instruction Objects
+                TableView<String> table = new TableView<String>();
+                popup.getContent().addAll(table);
+                popup.show(primaryStage);
             }
         });
         return help;
