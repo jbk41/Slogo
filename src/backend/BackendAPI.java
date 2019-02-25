32a1cc6c9a2e53f78a03648c815534859114d8b8
Justin Kim <jbk41@duke.edu>
2019-02-25 01:00:38 -0500

    changed API and implemented some methods

--- src/backend/BackendAPI.java
+++ src/backend/BackendAPI.java
@@ -1,4 +1,16 @@
 package backend;
 
 public interface BackendAPI {
+
+
+    /**
+     * interprets all of the code
+     * TODO: change void to returning a list of commands for front end to execute
+     * @param code
+     */
+    public void interpret(String code);
+
+    public void setLanguage(String language);
+
+
 }
--- src/backend/BackendModel.java
+++ src/backend/BackendModel.java
@@ -1,4 +1,30 @@
 package backend;
+import parser.ParseCleaner;
+import commands.GeneralCommand;
 
-public class BackendModel {
+public class BackendModel implements BackendAPI {
+
+
+    private VariableManager myVM;
+    private CommandTree myCT;
+    private ParseCleaner myPC;
+
+    public BackendModel(){
+        myVM = new VariableManager();
+    }
+
+    public void setLanguage(String language){
+        myPC = new ParseCleaner(language);
+    }
+
+    public void interpret(String text){
+        myCT = new CommandTree(text, myPC, myVM);
+    }
+
+    public void printTree(){
+        myCT.printTree();
+    }
+
+
+
 }
