4d48203cfa7a1a0f3a38d10e55b2f976427b3fda
Alejandro Meza <aam79@duke.edu>
2019-02-28 17:17:59 -0500

    Cleaned up reflection for Commands

--- src/backend/TestClass.java
+++ src/backend/TestClass.java
@@ -9,7 +9,7 @@ public class TestClass {
     public static void main (String[] args){
         BackendModel backend = new BackendModel();
         backend.setLanguage("English");
-        backend.interpret("make :hello 50 set :hi 80 fd :hi");
+        backend.interpret("make :hello 50 set :hello 80  set :hi 30 fd :hi");
         var map = backend.getBackendManager().getVariableManager().getVariableMap();
         for (String key : map.keySet()){
             System.out.println(key);
--- src/parser/CommandFactory.java
+++ src/parser/CommandFactory.java
@@ -38,27 +38,19 @@ public class CommandFactory {
     private GeneralCommand makeCommand(List<String> list) {
         try {
             Class clazz = Class.forName("commands." + list.get(0) + "Command");
-            try {
-                if (list.size() == 1) return (GeneralCommand) clazz.getConstructor(BackendManager.class).newInstance(BM);
-                else if (list.get(0).equals("Constant")) return (GeneralCommand) clazz.getConstructor(double.class).newInstance(Double.parseDouble(list.get(1)));
-                else return (GeneralCommand) clazz.getConstructor(BackendManager.class, String.class).newInstance(BM, list.get(1));
-            } catch (InstantiationException e) {
-                System.err.println("Error: Could not instantiate Constant Object with the given value");
-            } catch (NoSuchMethodException e) {
-                System.out.println("Could not instantiate Command " + list.get(0));
-            } catch (IllegalAccessException e) {
-                System.err.println("Error: " + e);
-            } catch (InvocationTargetException e) {
-                System.out.println("Error: " + e);
-            }
-        } catch (ClassNotFoundException e) {
-            System.out.println("The command " + list.get(0) + " does not exist");
-        }
+            if (list.size() == 1) return (GeneralCommand) clazz.getConstructor(BackendManager.class).newInstance(BM);
+            else if (list.get(0).equals("Constant")) return (GeneralCommand) clazz.getConstructor(double.class).newInstance(Double.parseDouble(list.get(1)));
+            else  return (GeneralCommand) clazz.getConstructor(BackendManager.class, String.class).newInstance(BM, list.get(1));
+        } catch (InstantiationException e) { System.out.println("The Command Could not be instantiated");
+        } catch (InvocationTargetException e) { e.printStackTrace();
+        } catch (NoSuchMethodException e) { System.out.println(e + ": This Command does not exist");
+        } catch (IllegalAccessException e) { e.printStackTrace();
+        } catch (ClassNotFoundException e) { System.out.println(e + ": This Command does not exist"); }
         return null;
     }
 
 
-    private ArrayList<String> parseText(String s) {
+        private ArrayList<String> parseText(String s) {
         ArrayList<String> cleanText = new ArrayList<>();
         String temp = myLanguages.getSymbol(s);
         cleanText.add(temp);
