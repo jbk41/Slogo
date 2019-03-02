ef290ba4743721b12a4b90f577676b7c2e35ded6
Mark Kang <sk531@duke.edu>
2019-03-02 16:45:37 -0500

    spacing works on console

--- src/Visualization/TurtleIDE.java
+++ src/Visualization/TurtleIDE.java
@@ -73,7 +73,7 @@ public class TurtleIDE extends Application {
             backend.setLanguage(language);
             backend.interpret(commands);
             turtle.moveTurtle(backend.getCommands());
-            console.setText(console.getText()+ "\r\n" + commands + "\r\n");
+            console.setText(console.getText() + "\r\n" + commands);
         }catch(NullPointerException ex){
             showError("Please Choose a Language");
         }
