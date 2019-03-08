801faa9e298c23b72d356475788aa24a57c6037c
Justin Kim <jbk41@duke.edu>
2019-03-08 16:34:56 -0500

    misc

--- src/parser/CommandFactory.java
+++ src/parser/CommandFactory.java
@@ -42,21 +42,24 @@ public class CommandFactory {
         try {
             String commandName = list.get(0);
 
-            Class clazz = Class.forName("commands." + commandName + "Command");
-
             if (commandName.equals(CONSTANT_COMMAND_NAME)) {
-                //System.out.println("Making constant command");
-                return (GeneralCommand) clazz.getConstructor(BackendManager.class, double.class).newInstance(myBM, Double.parseDouble(list.get(1)));
+                System.out.println("Making constant command");
+                return (GeneralCommand) Class.forName("commands." + commandName + "Command").getConstructor(BackendManager.class, double.class).newInstance(myBM, Double.parseDouble(list.get(1)));
             }
             else if (commandName.equals(VARIABLE_COMMAND_NAME)){
-                //System.out.println("Making variable command");
-                return (GeneralCommand) clazz.getConstructor(BackendManager.class, String.class).newInstance(myBM, list.get(1));
+                System.out.println("Making variable command");
+                return (GeneralCommand) Class.forName("commands." + commandName + "Command").getConstructor(BackendManager.class, String.class).newInstance(myBM, list.get(1));
             }
             else if (myLanguages.containsCommand(commandName)){
-                //System.out.println("Making regular command");
-                return (GeneralCommand) clazz.getConstructor(BackendManager.class).newInstance(myBM);
+                System.out.println("standard com " + commandName);
+                return (GeneralCommand) Class.forName("commands." + commandName + "Command").getConstructor(BackendManager.class).newInstance(myBM);
+            }
+            else if (myBM.containsCommand(commandName)){
+                System.out.println("user com " + commandName);
+                return new RunUserDefinedCommand(myBM, commandName);
             }
             else {
+                System.out.println("undef com " + commandName);
                 //System.out.println("Making undefined command");
                 return new UndefinedCommand(myBM, commandName);
             }
