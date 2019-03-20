1cb41040441d2f69284becbf83c754c9bf57a025
Alejandro Meza <aam79@duke.edu>
2019-03-20 00:03:00 -0400

    First Draft of my Part of JavaDoc Commenting

--- src/backend/BackendModel.java
+++ src/backend/BackendModel.java
@@ -20,42 +20,84 @@ public class BackendModel implements BackendAPI {
         myPC = new ParseCleaner(DEFAULT_LANGUAGE);
     }
 
+    /**
+     * Sets the language of the User Interface
+     * @param language
+     */
     public void setLanguage(String language){
         myPC = new ParseCleaner(language);
     }
 
+    /**
+     * Parses the text inputed by the user. This method creates a CommandTree, which then is updated, and then creates and Executable
+     * of its corresponding type.
+     * @param text - the user input text.
+     */
     public void interpret(String text){
         myCT = new CommandTree(text, myPC, myBM);
     }
 
+    /**
+     * Clears the CommandList whenever the user hits play.
+     */
     public void clearCommandList(){
         myBM.clearCommands();
     }
 
+    /**
+     * Returns the Variable Map from VariableManager. The map contains all the variables set by the user and its
+     * corresponding values.
+     */
     public Map<String, Double> getVarMap(){
         return Collections.unmodifiableMap(myBM.getVarMap());
     }
 
+    /**
+     * Adds an entry to the Variable Map.
+     * @param key - variable name
+     * @param val - variable value
+     */
     public void setVariable(String key, double val){
         myBM.setVariable(key, val);
     }
 
+    /**
+     * Returns a List of executables to visualize appropriately. Executables are created from the text after it
+     * is parsed by the backend.
+     * @return
+     */
     public List<Executable> getCommands(){
         return myBM.getCommands();
     }
 
+    /**
+     * Sets the new environment color to value of backgroundIndex
+     * @param backgroundIndex
+     */
     public void setBackgroundIndex(double backgroundIndex){
         myBM.getCurrentEnvironment().setBackgroundIndex(backgroundIndex);
     }
 
+    /**
+     * Sets the penColor to value of colorIndex
+     * @param colorIndex
+     */
     public void setColorIndex(double colorIndex){
         myBM.getCurrentEnvironment().setColorIndex(colorIndex);
     }
 
+    /**
+     * Sets pen size to the value of penSize
+     * @param penSize
+     */
     public void setPenSize(double penSize){
         myBM.getCurrentEnvironment().setPenSize(penSize);
     }
 
+    /**
+     * Sets shape of turtle to the value of shapeIndex
+     * @param shapeIndex
+     */
     public void setShapeIndex(double shapeIndex){
         myBM.getCurrentEnvironment().setShapeIndex(shapeIndex);
     }
--- src/backend/CommandManager.java
+++ src/backend/CommandManager.java
@@ -12,18 +12,37 @@ public class CommandManager {
         userCommands = new HashMap<>();
     }
 
+    /**
+     * Adds User-Deefined Command and its instruction to the user-Command Map
+     * @param name - name of user-dsefined command
+     * @param command - instruction of user-defined command
+     */
     public void addCommand(String name, UserDefinedCommand command){
         userCommands.put(name, command);
     }
 
+    /**
+     * Returns the instruction of the user-defined command from the Map of User-defined COmmands
+     * @param commandName - Name of User-defined command
+     */
     public UserDefinedCommand getCommand(String commandName){
         return userCommands.get(commandName);
     }
 
+    /**
+     * Checks if command is in Map of User-defined commands. If so, then true.
+     * @param commandName - Name of user-defined command
+     * @return
+     */
     public boolean containsCommand(String commandName){
         return userCommands.containsKey(commandName);
     }
 
+    /**
+     * Sets the maximum number of arguments for that command within the map.
+     * @param commandName - name of user defined command
+     * @param args - maximum number of arguments to set
+     */
     public void setMaxArgs(String commandName, int args){
         userCommands.get(commandName).setMaxArgs(args);
     }
